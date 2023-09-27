824441da9ac0753ff2193d1fa1a3f3cef03aaa10
Abdoul54 <abdelwahed.akhechane@gmail.com>
2023-09-26 18:06:05 -0700

    its over

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,18 @@
-# Using Puppet| Install Nginx server, setup and configuration
+# Automating project requirements using Puppet
 
 package { 'nginx':
-  ensure => 'installed'
+  ensure => installed,
+}
+
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/Abdoul54 permanent;',
 }
 
 file { '/var/www/html/index.html':
-  content => 'Hello World',
-}
-
-file_line { 'redirection-301':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  content => 'Hello World!',
 }
 
 service { 'nginx':
